8a3038b05796bef83bdfa1f3c8f4df3939432d1e
Kristin Ruben <kruben@gmail.com>
2016-07-27 14:53:05 -0400

    added google places map

--- app/assets/js/mycharts.js
+++ app/assets/js/mycharts.js
@@ -2,84 +2,24 @@ function laundry() {
   $dryerData = [];
   $washerData = [];
   $labelsArray = [];
-  $laundromatId = $('#laundo-id')[0].innerHTML;
+  $laundromatId = document.getElementById('laundo-id').innerHTML;
   var $response = $.get('/api/laundromats/',
     function(data) {
-      debugger;
-      $.each(data, function(k,v) {
-        $labelsArray.push(v.created_at);
+      $.each(data, function(k, v) {
+        $.each(v.washers, function(l, w) {
+          $labelsArray.push(w.created_at);
+        });
+      });
         $.each(data, function(k, v) {
-          $washerData.push(v.number_available);
-          $.each(data, function(k, v) {
-            $dryerData.push(v.number_available);
+          $.each(v.washers, function(l, w) {
+            $washerData.push(w.number_available);
+          });
+        });
+        $.each(data, function(k, v) {
+          $.each(v.dryers, function(x, y) {
+            $dryerData.push(y.number_available);
         });
       });
       laundryData($labelsArray, $washerData, $dryerData);
     });
-  });
-}
-function laundryData(soap, washerData, dryerData) {
-  var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: soap,
-    datasets: [
-      {
-        label: "WASHERS",
-        fill: true,
-        lineTension: 0.1,
-        backgroundColor: "#0192B4",
-        borderColor: "#0192B4",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: "rgba(75,192,192,1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 0,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 0,
-        pointHitRadius: 10,
-        data: washerData
-      },
-      {
-        label: "DRYERS",
-        fill: true,
-        lineTension: 0.1,
-        backgroundColor: "#C7CDCE",
-        borderColor: "#C7CDCE",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: "rgba(75,192,192,1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 0,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75,192,192,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 0,
-        pointHitRadius: 10,
-        data: dryerData
-      }
-    ]
-  },
-  options: {
-    scales: {
-
-            xAxes: [{
-              type: 'time',
-                time: {
-                    unit: 'day'
-                }
-            }]
-
-    }
   }
-});
-};
